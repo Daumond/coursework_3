2deccb9912abaa29479a37c106ac5a8db03dd01a
Dmitry <kde98@mail.ru>
2023-12-07 00:39:16 +0300

    added def mask_info

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+def mask_info(info):
+    if info.startswith("M") or info.startswith("V"):
+        card = info[:-16]
+        card_number = info[-16:]
+        masked_number = card_number[:4] + card_number[4:6] +  "** **** " + card_number[-4:]
+        return card + masked_number
+    elif info.startswith("Счет"):
+        account_number = info.split(" ")[-1]
+        masked_number = "**" + account_number[-4:]
+        return "Счет " + masked_number
+
+info1 = "Maestro 1596837868705199"
+info2 = "Счет 64686473678894779589"
+info3 = "Visa Platinum 1596837868705199"
+info4 = "Maestercard 1596837868705199"
+
+print(mask_info(info1))
+print(mask_info(info2))
+print(mask_info(info3))
+print(mask_info(info4))
