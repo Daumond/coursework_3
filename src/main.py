066494fbc6205eef70c27566fa2129e3af18bb1b
Dmitry <kde98@mail.ru>
2023-12-07 15:30:37 +0300

    add main

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import utils
+
+
+def main():
+    file_path = 'operations.json'
+    transactions = utils.read_json_file(file_path)
+
+    executed_transactions = [t for t in transactions if 'state' in t and t['state'] == 'EXECUTED']
+    sorted_transactions = sorted(executed_transactions, key=lambda x: x['date'], reverse=True)
+
+    for transaction in sorted_transactions[:5]:
+        print(utils.format_transaction(transaction))
+        print()
+
+if __name__ == "__main__":
+    main()
