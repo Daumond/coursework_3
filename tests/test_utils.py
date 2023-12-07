22ddabd433e17fb7686d4c0d27b472efe69f456e
Dmitry <kde98@mail.ru>
2023-12-07 14:15:57 +0300

    fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,7 @@ def test_mask_info():
     assert utils.mask_info("МИР 6381702861749111") == "МИР 6381 70** **** 9111"
 
 
-def format_transaction_date():
+def test_format_transaction_date():
     assert utils.format_transaction_date("2019-02-14T17:38:09.910336") == "14.02.2019"
     assert utils.format_transaction_date("2018-08-14T05:42:30.104666") == "14.08.2018"
+    assert utils.format_transaction_date("2019-05-17T01:50:00.166954") == "17.05.2019"
